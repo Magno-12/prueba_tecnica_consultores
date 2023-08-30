7cb35d535bc91c5ea4ff2aaed97fc3e1acff6576
Magno Martinez <66977118+Magno-12@users.noreply.github.com>
2023-08-30 17:54:03 -0500

    docstring in endpoint

--- apps/property_management/views/predio_view.py
+++ apps/property_management/views/predio_view.py
@@ -19,17 +19,78 @@ class PredioViewSet(GenericViewSet):
         return [IsOwner()]
 
     def list(self, request):
+        """
+        Endpoint: Listado de predios
+        ---
+        Body: N/A
+        ---
+        Response:
+        [
+            {
+                "id": 1,
+                "nombre_direccion": "Calle principal 123",
+                "tipo": "Casa",
+                "numero_catastral": "1234567890",
+                "numero_matricula": "ABC1234567890",
+                "propietarios": [1, 2, 3]
+            },
+        ]
+        """
         queryset = self.get_queryset()
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
     def create(self, request):
+        """
+        Endpoint: Crear un nuevo predio
+        ---
+        Body:
+        {
+            "nombre_direccion": "Calle principal 123",
+            "tipo": "Casa",
+            "numero_catastral": "1234567890",
+            "numero_matricula": "ABC1234567890",
+            "propietarios": [1, 2, 3]
+        }
+        ---
+        Response:
+        {
+            "id": str,
+            "nombre_direccion": "Calle principal 123",
+            "tipo": "Casa",
+            "numero_catastral": "1234567890",
+            "numero_matricula": "ABC1234567890",
+            "propietarios": [1, 2, 3]
+        }
+        """
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def partial_update(self, request, pk=None):
+        """
+        Endpoint: Actualizar detalles de un predio
+        ---
+        Body:
+        {
+            "nombre_direccion": "Calle principal 456",
+            "tipo": "Apartamento",
+            "numero_catastral": "0987654321",
+            "numero_matricula": "ZYX0987654321",
+            "propietarios": [2, 3, 4]
+        }
+        ---
+        Response:
+        {
+            "id": str,
+            "nombre_direccion": "Calle principal 456",
+            "tipo": "Apartamento",
+            "numero_catastral": "0987654321",
+            "numero_matricula": "ZYX0987654321",
+            "propietarios": [2, 3, 4]
+        }
+        """
         predio = self.get_object()
         if request.user.owner not in predio.propietarios.all():
             return Response({"detail": "No tienes permiso para editar este predio."},
@@ -42,9 +103,19 @@ class PredioViewSet(GenericViewSet):
         return Response(serializer.data)
 
     def destroy(self, request, pk=None):
+        """
+        Endpoint: Eliminar un predio
+        ---
+        Body: N/A
+        ---
+        Response:
+        {
+            "detail": "Predio eliminado con éxito."
+        }
+        """
         predio = self.get_object()
         if request.user.owner not in predio.propietarios.all():
-            return Response({"detail": "No tienes permiso para eliminar este predio."}, 
+            return Response({"detail": "No tienes permiso para eliminar este predio."},
                 status=status.HTTP_403_FORBIDDEN
             )
 
